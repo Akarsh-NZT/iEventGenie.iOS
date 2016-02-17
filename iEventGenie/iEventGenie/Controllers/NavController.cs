d663eb9347bad492e3bd53a1804c474def532ae9
Akarsh Deep <akarsh.deep@zooom-it.com>
2016-02-17 17:27:07 +0530

    Project Start

---
+++ iEventGenie/iEventGenie/Controllers/NavController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+
+namespace iEventGenie
+{
+	public partial class NavController : UINavigationController
+	{
+		public NavController() : base((string)null, null)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
